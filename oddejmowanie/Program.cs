3bdca915d52692064a6fe366691911a6337170e2
Igor <igorkris719@gmail.com>
2024-03-10 17:25:36 +0100

    nie wiem jak zrobić by było 10 10,9 10,9,8 itd aż do 100

--- oddejmowanie/Program.cs
+++ oddejmowanie/Program.cs
@@ -1,9 +1,21 @@
-﻿for (int a = 10; a >= 0; a--)
+﻿/*for (int a = 10; a >= -99; a--)
 {
     for (int b = 10; a <= b;)
     {
         Console.Write(b + ",");
         b = b - 1;
+    }
+    Console.WriteLine();
+}*/
+int b = 10;
+int a = 10;
+while ( a >= -99 )
+{
+    a--;
+    while ( a <= b)
+    {
+        Console.Write(b + ",");
+        b --;
     }
     Console.WriteLine();
 }
